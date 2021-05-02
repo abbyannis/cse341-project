c78c8ccaf9e40635cb42d06164c610967645eeec
Abby Annis <abbyannis@gmail.com>
2021-05-01 19:41:49 -0600

    commit

--- prove02.js
+++ prove02.js
@@ -19,6 +19,6 @@ app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found', path: '404'});
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
