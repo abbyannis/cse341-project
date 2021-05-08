bf81273d036f1cbec4f3f561416773dca6b9c41e
Abby Annis <abbyannis@gmail.com>
2021-05-08 11:21:11 -0600

    fixed routing when nav from team activities to prove assignment 2

--- routes/teamRoutes/index.js
+++ routes/teamRoutes/index.js
@@ -5,6 +5,9 @@ routes
     .use('/02', require('./ta02'))
     .use('/03', require('./ta03'))
     .use('/04', require('./ta04'))
+    .get('/proveAssignments/02', (req, res, next) => {
+        res.redirect('../../proveAssignments/02');
+    })
     .get('/', (req, res, next) => {
         res.render('pages/teamActivities/', {
             pagetTitle: 'Team Activities',
