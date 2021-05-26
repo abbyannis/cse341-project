839cade483298d0a208722388e1838b501d40212
Abby Annis <abbyannis@gmail.com>
2021-05-25 19:37:21 -0600

    adding favicon

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ require('dotenv').config({path: path.join(__dirname, '.env')});
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes')
+const favicon = require('serve-favicon');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
+app.use(favicon(__dirname + '/public/images/favicon.png'));
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
