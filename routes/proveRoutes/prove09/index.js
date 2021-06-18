ccec46a932d628bfe4d94c210b54d045d0827f7d
Abby Annis <abbyannis@gmail.com>
2021-06-18 15:36:22 -0600

    Prove 09 fixes

--- routes/proveRoutes/prove09/index.js
+++ routes/proveRoutes/prove09/index.js
@@ -2,18 +2,6 @@ const routes = require('express').Router();
 
 const shopController = require('../../../controllers/prove08.1/shop');
 
-var cors = require('cors');
-var bodyParser = require('body-parser');
-
-//enables cors
-routes.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
-
 routes
     .get('/', require('./pokemon'));
 
